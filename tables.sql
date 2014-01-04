cef258ac076ffa444aef5fd3c4a64932dbad9b32
Artem <altruer@gmail.com>
2014-01-04 18:00:09 +0400

    Yii init

--- tables.sql
+++ tables.sql
@@ -6,21 +6,22 @@ CREATE SCHEMA `autohelp`
 USE autohelp;
 
 CREATE TABLE `order` (
-  `order_id`      INT(10) UNSIGNED    NOT NULL AUTO_INCREMENT,
-  `email`         VARCHAR(256),
-  `phone`         VARCHAR(16),
-  `name`          VARCHAR(64),
-  `second_name`   VARCHAR(64),
-  `surname`       VARCHAR(64),
-  `vin`           VARCHAR(17),
-  `number`        VARCHAR(16),
-  `trademark`     VARCHAR(64),
-  `model`         VARCHAR(64),
-  `color`         VARCHAR(32),
-  `card_id`       VARCHAR(64),
-  `action_tag_id` INT(10) UNSIGNED    NOT NULL,
-  `activated`     TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-  `timestamp`     DATETIME            NOT NULL,
+  `order_id`         INT(10) UNSIGNED    NOT NULL AUTO_INCREMENT,
+  `email`            VARCHAR(256),
+  `phone`            VARCHAR(16),
+  `name`             VARCHAR(64),
+  `second_name`      VARCHAR(64),
+  `surname`          VARCHAR(64),
+  `vin`              VARCHAR(17),
+  `number`           VARCHAR(16),
+  `trademark`        VARCHAR(64),
+  `model`            VARCHAR(64),
+  `color`            VARCHAR(6),
+  `delivery_address` VARCHAR(256),
+  `card_id`          VARCHAR(64),
+  `action_tag_id`    INT(10) UNSIGNED    NOT NULL,
+  `activated`        TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  `timestamp`        DATETIME            NOT NULL,
 
 
   PRIMARY KEY (`order_id`),
@@ -37,10 +38,26 @@ CREATE TABLE `order` (
 CREATE TABLE `action_tag` (
   `action_tag_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title`         VARCHAR(128)     NOT NULL,
-  `description`   VARCHAR(256)     NOT NULL,
+  `description`   VARCHAR(256),
   PRIMARY KEY (`action_tag_id`),
   UNIQUE KEY `title` (`title`),
   UNIQUE KEY `description` (`description`)
 )
   ENGINE =InnoDB
   DEFAULT CHARSET =utf8;
+
+
+CREATE TABLE `services` (
+  `service_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`         VARCHAR(128)     NOT NULL,
+  `description`   VARCHAR(256),
+  PRIMARY KEY (`action_tag_id`),
+  UNIQUE KEY `title` (`title`),
+  UNIQUE KEY `description` (`description`)
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8;
+
+
+
+/*TODO add realtime services*/
