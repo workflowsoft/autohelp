1ba951af1d4c618a1738193ab88c3466263b9124
altruer <altruer@gmail.com>
2014-01-05 14:59:31 +0400

    More tables

--- tables.sql
+++ tables.sql
@@ -46,7 +46,7 @@ CREATE TABLE `action_tag` (
   DEFAULT CHARSET =utf8;
 
 
-CREATE TABLE `services` (
+CREATE TABLE `service` (
   `service_id`  INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title`       VARCHAR(128)     NOT NULL,
   `description` VARCHAR(256),
@@ -59,8 +59,8 @@ CREATE TABLE `services` (
 
 CREATE TABLE `partner` (
   `partner_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR (128),
-  `address`VARCHAR(256),
+  `name`       VARCHAR(128),
+  `address`    VARCHAR(256),
 
   PRIMARY KEY (`partner_id`),
   UNIQUE KEY (`name`)
@@ -68,10 +68,104 @@ CREATE TABLE `partner` (
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-/* mtm */
-/*CREATE TABLE `partner_service` (
 
+CREATE TABLE `partner2service` (
+  `partner2service_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `partner_id`         INT(10) UNSIGNED NOT NULL,
+  `service_id`         INT(10) UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`partner2service_id`),
+  FOREIGN KEY (`partner_id`) REFERENCES `partner` (`partner_id`),
+  FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`)
 )
-*/
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-/*TODO add realtime services*/
+
+CREATE TABLE `order2action_tag` (
+  `order2action_tag_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `order_id`            INT(10) UNSIGNED NOT NULL,
+  `action_tag_id`       INT(10) UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`order2action_tag_id`),
+  FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`),
+  FOREIGN KEY (`action_tag_id`) REFERENCES `action_tag` (`action_tag_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `card_parcel` (
+  `card_parcel_id` INT(10) UNSIGNED     NOT NULL AUTO_INCREMENT,
+  `start_card_id`  VARCHAR(64)          NOT NULL,
+  `end_card_id`    VARCHAR(64)          NOT NULL,
+  `series`         ENUM ('A', 'B', 'C') NOT NULL,
+  `comment`        VARCHAR(256),
+
+  PRIMARY KEY (`card_parcel_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `request` (
+  `request_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `status`     ENUM ('opened', 'in_progress', 'resolved', 'rejected'),
+  `comment`    VARCHAR(256),
+  `user_id`    INT(10) UNSIGNED NOT NULL,
+  `timestamp`  DATETIME,
+
+  PRIMARY KEY (`request_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `user` (
+  `user_id`     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(64),
+  `second_name` VARCHAR(64),
+  `surname`     VARCHAR(64),
+
+  PRIMARY KEY (`user_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `role` (
+  `role_id`     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title`       VARCHAR(128),
+  `description` VARCHAR(256),
+
+  PRIMARY KEY (`role_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `user2role` (
+  `user2role_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id`      INT(10) UNSIGNED NOT NULL,
+  `role_id`      INT(10) UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`user2role_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+
+CREATE TABLE `request2partner2service` (
+  `request2partner2services_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `request_id`                  INT(10) UNSIGNED NOT NULL,
+  `partner2service_id`          INT(10) UNSIGNED NOT NULL,
+
+  PRIMARY KEY (`request2partner2services_id`),
+  FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`),
+  FOREIGN KEY (`partner2service_id`) REFERENCES `partner2service` (`partner2service_id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
