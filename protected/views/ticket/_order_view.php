24f2885db92da3b66f391cc66aaf3444f548d8ae
Artyom <altruer@gmail.com>
2014-01-22 23:40:49 +0400

    [+] card number

--- protected/views/ticket/_order_view.php
+++ protected/views/ticket/_order_view.php
@@ -1,23 +1,43 @@
 <?php
-    $this->widget('bootstrap.widgets.TbDetailView',array(
-	'data'=>$model,
-	'attributes'=>array(
-//		'id',
+
+$this->widget('bootstrap.widgets.TbDetailView',array(
+    'data'=>$model,
+    'attributes'=>array(
+        'id',
+        array(
+            'name' => 'email',
+            'type' => 'email'
+        ),
         'phone',
-        'email',
-		'first_name',
-		'middle_name',
-		'last_name',
-		'vin',
-		'grn',
-		'ts_make',
-		'ts_model',
-		'ts_color',
-//		'card_delivery_address',
-		'card_id',
-//		'activation_start',
-//		'activation_end',
-//		'delivery_coords',
-//		'delivery_street',
-	),
-)); ?>
+        'description',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'vin',
+        'grn',
+        'ts_make',
+        'ts_model',
+        'ts_color',
+        'card_delivery_address',
+        array(
+            'label' => 'Номер карты',
+            'type' => 'raw',
+            'value' => isset($model->card->number) ? CHtml::encode($model->card->number) : '',
+        ),
+        array(
+            'name' => 'activation_start',
+            'type' => 'date'
+        ),
+        array(
+            'name' => 'activation_end',
+            'type' => 'date'
+        ),
+        'delivery_street',
+        array(
+            'name' => 'delivered',
+            'type' => 'boolean'
+        ),
+    ),
+));
+
+?>
